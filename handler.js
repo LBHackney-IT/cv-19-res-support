d6e902d6b97ea245ec6dad86b2c95abba424e94a
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-03-24 09:09:34 +0000

    Added logic for support volunteer endpoints.

--- handler.js
+++ handler.js
@@ -66,3 +66,69 @@ module.exports.getResidentSupportRequests = (event, context, callback) => {
       })
     })
 };
+
+module.exports.createSupportVolunteerRecord = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const data = JSON.parse(event.body);
+  //let validationErrors = validator.validate(data);
+  if(validationErrors.length == 0)
+  {
+    db.insert('public.support_volunteers', data)
+      .then(res => {
+        callback(null,{
+          statusCode: 201,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+          },
+          body: JSON.stringify(res)
+        })
+      })
+      .catch(e => {
+        callback(null,{
+          statusCode: e.statusCode || 500,
+          headers:{
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+          },
+          body: "Error creating volunteer record " + e
+        })
+      })
+  }
+  else
+  {
+    callback(null,{
+      statusCode: 400,
+      headers:{
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify(validationErrors)
+    })
+  }
+};
+
+module.exports.getSupportVolunteerRecords = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  db.getAll('public.support_volunteers')
+    .then(res =>{
+      callback(null,{
+        statusCode: 200,
+        headers:{
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify(res)
+      })
+    })
+    .catch(e => {
+      callback(null,{
+        statusCode: e.statusCode || 500,
+        headers:{
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: "Error retrieving data " + e
+      })
+    })
+};
